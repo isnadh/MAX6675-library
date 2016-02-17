05e702c08c18827bd45de1b0412060b8d67d67ec
isnadh <isnadh@users.noreply.github.com>
2016-02-17 21:52:01 +0100

    Update max6675.cpp
    The minimum conversion time for the chip is specified to be 220ms, then data is available on the NGT of the clock and not PGT.

--- max6675.cpp
+++ max6675.cpp
@@ -1,5 +1,6 @@
 // this library is public domain. enjoy!
 // www.ladyada.net/learn/sensors/thermocouple
+// corrected by me: engineerscookbook191@gmail.com
 
 #ifdef __AVR
   #include <avr/pgmspace.h>
@@ -8,9 +9,10 @@
 #endif
 #include <util/delay.h>
 #include <stdlib.h>
-#include "max6675.h"
+#include "max6675edit.h"
 
-MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) {
+MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) 
+{
   sclk = SCLK;
   cs = CS;
   miso = MISO;
@@ -22,12 +24,18 @@ MAX6675::MAX6675(int8_t SCLK, int8_t CS, int8_t MISO) {
 
   digitalWrite(cs, HIGH);
 }
-double MAX6675::readCelsius(void) {
+
+
+double MAX6675::readCelsius(void) 
+{
 
   uint16_t v;
 
-  digitalWrite(cs, LOW);
-  _delay_ms(1);
+//digitalWrite(cs, LOW);_delay_ms(1);digitalWrite(cs,HIGH);_delay_ms(220);
+
+
+_delay_ms(220);
+digitalWrite(cs, LOW);
 
   v = spiread();
   v <<= 8;
@@ -35,9 +43,9 @@ double MAX6675::readCelsius(void) {
 
   digitalWrite(cs, HIGH);
 
-  if (v & 0x4) {
-    // uh oh, no thermocouple attached!
-    return NAN; 
+  if (v & 0x4) 
+  {
+    return NAN;// uh oh, no thermocouple attached! 
     //return -100;
   }
 
@@ -46,26 +54,29 @@ double MAX6675::readCelsius(void) {
   return v*0.25;
 }
 
-double MAX6675::readFahrenheit(void) {
+
+double MAX6675::readFahrenheit(void) 
+{
   return readCelsius() * 9.0/5.0 + 32;
 }
 
-byte MAX6675::spiread(void) { 
-  int i;
-  byte d = 0;
+
+byte MAX6675::spiread(void) 
+{ 
+  int i;byte d = 0;
 
   for (i=7; i>=0; i--)
   {
-    digitalWrite(sclk, LOW);
-    _delay_ms(1);
-    if (digitalRead(miso)) {
-      //set the bit to 0 no matter what
-      d |= (1 << i);
+    delayMicroseconds(100);
+    digitalWrite(sclk, HIGH);  //its HIGH not LOW as with ladyada library
+    delayMicroseconds(100);
+    if (digitalRead(miso)) 
+    { 
+      d |= (1 << i);//set the bit to 0 no matter what 
     }
+    digitalWrite(sclk, LOW);//its LOW not HIGH as with ladyada library  
+  }   
 
-    digitalWrite(sclk, HIGH);
-    _delay_ms(1);
-  }
-
-  return d;
+  return d;  
 }
+
